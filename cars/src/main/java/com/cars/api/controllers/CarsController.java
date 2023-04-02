c336b3033c245ac64cc98e590923eb0728518dc9
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-04-02 13:41:43 -0300

    :sparkles: feat: Implements the cars repository

--- cars/src/main/java/com/cars/api/controllers/CarsController.java
+++ cars/src/main/java/com/cars/api/controllers/CarsController.java
@@ -1,21 +1,24 @@
 package com.cars.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarDTO;
+import com.cars.api.models.Car;
+import com.cars.api.repositories.CarsRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarsController {
 
+    @Autowired
+    private CarsRepository repository;
+
     @PostMapping
     public void create(@RequestBody CarDTO req) {
-        System.out.println(req.modelo());
-        System.out.println(req.dataFabricacao());
-        System.out.println(req.valor());
-        System.out.println(req.anoModelo());
+        repository.save(new Car(req));
     }
 }
